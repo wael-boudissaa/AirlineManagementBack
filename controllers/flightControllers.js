b971d6622f686fee9b504f3c2a87e9d0a67fcad3
wael-boudissa <waelb20102@gmail.com>
2023-08-16 17:35:19 +0100

    relate database to back

--- controllers/flightControllers.js
+++ controllers/flightControllers.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const app = express();
-const getFlights = asyncHandler(async (req, res) => {});
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost", // Change this to your database host
+  user: "root",
+  password: "wael",
+  database: "flightmanagement",
+});
+const getFlights = asyncHandler(async (req, res) => {
+  query = "SELECT * from  flight";
+  connection.query(query, (err, result) => {
+    if (err) return err;
+    res.send(result);
+  });
+});
 module.exports = { getFlights };
